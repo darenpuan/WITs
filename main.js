a69ec07d6a9aaa1dd9c5e2adced5f453a953db65
Daren Puan <darenpuan@gmail.com>
2018-09-11 20:21:01 +0800

    2

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 function generateBarCode() {
     var serialNo = document.getElementById('serialNo').value;
-    var link = "https://darenpuan.github.io/" + serialNo + "/"
+    var link = "https://darenpuan.github.io/WITs/" + serialNo + ".html"
     var url = 'https://api.qrserver.com/v1/create-qr-code/?data=' + link + '&amp;size=100x100';
     return url;
 }
 
 function generateBarCodeC() {
     var serialNo = $('#serialNo').val();
-    var link = "https://darenpuan.github.io/" + serialNo + "/"
+    var link = "https://darenpuan.github.io/WITs/" + serialNo + ".html"
     var url = 'https://api.qrserver.com/v1/create-qr-code/?data=' + link + '&amp;size=100x100';
     $('#barcode').attr('src', url);
 }
